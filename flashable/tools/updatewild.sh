ef7d1f0dc080a8c1adbcfc2dedf81852c05d4b5e
Emmanuel Utomi <emmanuelutomi@gmail.com>
2015-04-17 23:10:18 -0500

    WildKernel: remove extra space in installer

--- flashable/tools/updatewild.sh
+++ flashable/tools/updatewild.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #DO NOT EDIT FILE - AUTO GENERATED FOR f240
 latestversion=2
-latestdate=150416
-latestdateliteral='April 16 2015'
+latestdate=150417
+latestdateliteral='April 17 2015'
 latestDL=http://forum.xda-developers.com/optimus-g-pro/orig-development/kernel-wildkernel-build-1-featurlicous-t3069629
 input="$1"
 if [[ -z "$input" ]]
